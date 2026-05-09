7e93dfa73a1f08beae7c5c1c078ed6a6912adfba
Ali4441 <amd444147@gmail.com>
2026-05-10 01:34:45 +0530

    fix: resolve cors network error

--- server/server.js
+++ server/server.js
@@ -19,16 +19,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // ─── Security Middleware ───────────────────────────────────────────────────
-// app.use(helmet());
+app.use(helmet());
 
-app.use(cors({
-  origin: [
-    process.env.CLIENT_URL,
-    "http://localhost:5173"
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+app.use(cors());
 
 
 app.use(express.json({ limit: "10kb" }));
