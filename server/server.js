2ee7ccc06055d036ebe77c968187cf9af495c9a2
Ali4441 <amd444147@gmail.com>
2026-05-10 03:29:46 +0530

    fix: resolve cors network error

--- server/server.js
+++ server/server.js
@@ -20,16 +20,31 @@ const PORT = process.env.PORT || 5000;
 
 // ─── Security Middleware ───────────────────────────────────────────────────
 app.use(helmet());
-
 app.use(cors({
-  origin: [
-    process.env.CLIENT_URL,
-    "http://localhost:5173"
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  origin: function (origin, callback) {
+
+    const allowedOrigins = [
+      "http://localhost:5173",
+      "https://portfolio-six-delta-22.vercel.app",
+      "https://portfolio-1grv.vercel.app"
+    ];
+
+    // allow requests with no origin (thunder/postman)
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
 
+app.options("*", cors());
 
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
