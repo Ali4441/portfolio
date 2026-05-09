5219740087113df574d3a1be79a85baad7a83fdc
Ali4441 <amd444147@gmail.com>
2026-05-10 02:31:09 +0530

    fix: resolve cors network error

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -18,7 +18,7 @@ const Contact = () => {
       // await axios.post("http://localhost:5000/api/contact", form);
       console.log("API URL:", import.meta.env.VITE_API_URL);
       await axios.post(
-        `${import.meta.env.VITE_API_URL}api/contact`,
+        `${import.meta.env.VITE_API_URL}/api/contact`,
         form
       );
       setStatus("success");
