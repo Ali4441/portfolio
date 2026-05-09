d4bb0bd3367fa736d9d3b3954bda3311c5a8dfaa
Ali4441 <amd444147@gmail.com>
2026-05-10 00:52:56 +0530

    fix: simplify cors for deployed frontend

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use(helmet());
 
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
